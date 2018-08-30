dc0ea1a8fdbbf48a1f317119aa54e7654dc8f80a
Sean Murakami <murakami.sean@yahoo.com>
2018-08-30 10:48:05 -0700

    created a variable with a boolean value that changes whether the clock is running or not && clear the interval if the watch is running when the button is clicked && swapped the play icon for a pause icon when clicked the first time

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-let $startButton = document.querySelector('button')
+let watchTime = false
+
+let $startButton = document.querySelector('#startClock')
 let $changedDigit = document.querySelector('p')
+
+let x;
 
 function update() {
   let countUp = parseInt($changedDigit.textContent, 10)
+  if (watchTime === true){
   countUp += 1
   $changedDigit.textContent = countUp
+  }
 }
 
 function showNumber() {
-  setInterval(update, 1000)
+  watchTime = !watchTime
+  clearInterval(x)
+  x = setInterval(update, 1000)
+  if (watchTime === true) {
+    $startButton.textContent = "Pause"
+  } else {
+    $startButton.textContent = "Resume"
+  }
 }
 
 $startButton.addEventListener('click', showNumber)
