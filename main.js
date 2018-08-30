a60df2c4354e769105e66ce7a7bf957983a0ee95
Sean Murakami <murakami.sean@yahoo.com>
2018-08-29 17:46:32 -0700

    #1 updated main.js to pass the function name to setInterval instead of the function expression && updated the button to change color when you hover over it and click it

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let $startbutton = document.querySelector('button')
 let $changeddigit = document.querySelector('p')
 
-let x = function update() {
+function update() {
   let countUp = parseInt($changeddigit.textContent, 10)
   countUp += 1
   $changeddigit.textContent = countUp
 }
 
 function showNumber() {
-  setInterval(x, 1000)
+  setInterval(update, 1000)
 }
 
 $startbutton.addEventListener('click', showNumber)
